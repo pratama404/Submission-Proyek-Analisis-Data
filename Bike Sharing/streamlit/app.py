5f21a7094bc7c7fa533d2a2d7cfe12ec0ee2e8cc
Ageng Putra Pratama <65237429+pratama404@users.noreply.github.com>
2024-01-09 20:14:12 +0700

    add

--- Bike Sharing/streamlit/app.py
+++ Bike Sharing/streamlit/app.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 
 @st.cache_resource
 def load_data():
-    data = pd.read_csv("Bike Sharing/dataset/hour.csv")
+    data = pd.read_csv("../Bike Sharing/dataset/hour.csv")
     return data
 
 
